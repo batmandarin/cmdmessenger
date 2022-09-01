ffc7628b8bda03917685604777c27233b363151b
batmandarin <107263949+batmandarin@users.noreply.github.com>
2022-09-01 14:38:21 +0900

    Add files via upload

--- .github/workflows/Form1.Designer.cs
+++ .github/workflows/Form1.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace BasicControls
+﻿namespace FocusApp
 {
-    partial class InputForm
+    partial class FocusForm
     {
         /// <summary>
         /// 필수 디자이너 변수입니다.
@@ -30,72 +30,76 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.txtName = new System.Windows.Forms.TextBox();
-            this.btnOK = new System.Windows.Forms.Button();
+            this.btnlnput = new System.Windows.Forms.Button();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.txtWeight = new System.Windows.Forms.TextBox();
             this.lblResult = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Location = new System.Drawing.Point(48, 28);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(272, 21);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "이름을 입력하고 버튼을 눌러주세요.";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "학번";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(46, 61);
+            this.label2.Location = new System.Drawing.Point(48, 74);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.Size = new System.Drawing.Size(29, 12);
             this.label2.TabIndex = 1;
-            this.label2.Text = "이름 : ";
+            this.label2.Text = "체중";
             // 
-            // txtName
+            // btnlnput
             // 
-            this.txtName.ImeMode = System.Windows.Forms.ImeMode.Hangul;
-            this.txtName.Location = new System.Drawing.Point(115, 58);
-            this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(100, 21);
-            this.txtName.TabIndex = 2;
-            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            this.btnlnput.Location = new System.Drawing.Point(12, 126);
+            this.btnlnput.Name = "btnlnput";
+            this.btnlnput.Size = new System.Drawing.Size(75, 23);
+            this.btnlnput.TabIndex = 2;
+            this.btnlnput.Text = "입력";
+            this.btnlnput.UseVisualStyleBackColor = true;
+            this.btnlnput.Click += new System.EventHandler(this.btnlnput_Click);
             // 
-            // btnOK
+            // txtNum
             // 
-            this.btnOK.Location = new System.Drawing.Point(105, 127);
-            this.btnOK.Name = "btnOK";
-            this.btnOK.Size = new System.Drawing.Size(75, 23);
-            this.btnOK.TabIndex = 3;
-            this.btnOK.Text = "확인";
-            this.btnOK.UseVisualStyleBackColor = true;
-            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            this.txtNum.Location = new System.Drawing.Point(140, 25);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(100, 21);
+            this.txtNum.TabIndex = 0;
+            this.txtNum.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNum_KeyPress);
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.Location = new System.Drawing.Point(140, 71);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(100, 21);
+            this.txtWeight.TabIndex = 1;
+            this.txtWeight.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtWeight_KeyPress);
             // 
             // lblResult
             // 
-            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.lblResult.Location = new System.Drawing.Point(12, 187);
+            this.lblResult.Location = new System.Drawing.Point(123, 126);
             this.lblResult.Name = "lblResult";
-            this.lblResult.Size = new System.Drawing.Size(260, 20);
-            this.lblResult.TabIndex = 4;
-            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.lblResult.Click += new System.EventHandler(this.lblResult_Click);
+            this.lblResult.Size = new System.Drawing.Size(160, 23);
+            this.lblResult.TabIndex = 5;
             // 
-            // InputForm
+            // FocusForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
             this.Controls.Add(this.lblResult);
-            this.Controls.Add(this.btnOK);
-            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtWeight);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.btnlnput);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Name = "InputForm";
-            this.Text = "입력처리";
+            this.Name = "FocusForm";
+            this.Text = "포커스활용(입력)";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -105,8 +109,9 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txtName;
-        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnlnput;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.TextBox txtWeight;
         private System.Windows.Forms.Label lblResult;
     }
 }
--- .github/workflows/Form1.cs
+++ .github/workflows/Form1.cs
@@ -8,33 +8,30 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace BasicControls
+namespace FocusApp
 {
-    public partial class InputForm : Form
+    public partial class FocusForm : Form
     {
-        public InputForm()
+        public FocusForm()
         {
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void txtNum_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //엔터키(\r\n)를 누르면 다음 입력 컨트롤로 포커스 이동
+            if (e.KeyChar == '\r') txtWeight.Focus();
+        }
+
+        private void txtWeight_KeyPress(object sender, KeyPressEventArgs e)
         {
 
         }
 
-        private void txtName_TextChanged(object sender, EventArgs e)
+        private void btnlnput_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void btnOK_Click(object sender, EventArgs e)
-        {
-            lblResult.Text = txtName.Text + "님, 환영합니다!";
-        }
-
-        private void lblResult_Click(object sender, EventArgs e)
-        {
-
+            //엔터키(\r\n)를 누르면 입력 내용을 출력 레이블에 표시
+            lblResult.Text = "학번:" + txtNum.Text + ", 체중:" + txtWeight.Text;
         }
     }
 }
