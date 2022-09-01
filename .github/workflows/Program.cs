59d9fbbe08ab15585b262dc92d01679aff47cb8b
batmandarin <107263949+batmandarin@users.noreply.github.com>
2022-09-01 14:37:43 +0900

    Add files via upload

--- .github/workflows/Program.cs
+++ .github/workflows/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace BasicControls
+namespace controls
 {
     static class Program
     {
@@ -16,7 +16,7 @@ namespace BasicControls
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new InputForm());
+            Application.Run(new Form1());
         }
     }
 }
